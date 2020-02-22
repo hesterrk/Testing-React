299b721a4c5398a5fb843e0fa360c393510d15ca
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-22 13:32:54 +0000

    set up

--- star-wars-app/src/setupTests.js
+++ star-wars-app/src/setupTests.js
@@ -2,4 +2,20 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, wait } from '@testing-library/react';
+
+import StarWarsCharacters from './components/StarWarsCharacters';
+import { getData as mockGetData } from './api';
+
+
+jest.mock('./api');
+
+
+test('the entire flow', async () => {
+
+
+
+
+});
